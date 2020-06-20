4346935b5967c8165e64b3c26dc4cc054607e294
ik-fib <ik11235@gmail.com>
2020-06-20 17:33:50 +0900

    レイアウトを修正

--- js/main.js
+++ js/main.js
@@ -1,25 +1,76 @@
+function getDomein(str) {
+    try {
+        const parser = new URL(str);
+        return parser.hostname;
+    } catch (e) {
+        if (e instanceof TypeError) {
+            return "";
+        } else {
+            throw e;
+        }
+    }
+}
+
+function escape_html(string) {
+    // https://qiita.com/saekis/items/c2b41cd8940923863791
+    if (typeof string !== 'string') {
+        return string;
+    }
+    return string.replace(/[&'`"<>]/g, function (match) {
+        return {
+            '&': '&amp;',
+            "'": '&#x27;',
+            '`': '&#x60;',
+            '"': '&quot;',
+            '<': '&lt;',
+            '>': '&gt;',
+        }[match]
+    });
+}
+
 window.onload = function () {
     chrome.sessions.getDevices(function (results) {
         const main = document.getElementById('main');
         console.log(main);
         results.forEach(function (result) {
-            let device_name = result.deviceName;
+            const device_name = result.deviceName;
             console.log(device_name);
-            main.insertAdjacentHTML('beforeend', `<h2>${device_name}</h2>`);
+            main.insertAdjacentHTML('beforeend', `<h3>${device_name}</h3>`);
 
-            let sessions = result.sessions;
-            sessions.forEach(function (session) {
-                main.insertAdjacentHTML('beforeend', `<h3>Session Id${session.window.sessionId}</h3>`);
-                let tabs = session.window.tabs;
-                main.insertAdjacentHTML('beforeend', `<h3>${tabs.length}個のタブ</h3>`);
-                tabs.forEach(function (tab) {
+            const sessions = result.sessions;
+            for (let i = 0; i < sessions.length; i++) {
+                const session = sessions[i];
+                const tabs = session.window.tabs;
+
+                const tabs_html = tabs.map(function (tab) {
                     const url = (tab.pendingUrl ? tab.pendingUrl : tab.url);
-                    console.log(tab.title + " " + url + " " + tab.sessionId);
-                    main.insertAdjacentHTML('beforeend', `<p><a href="${url}" target="_blank">${tab.title}</p>`);
-                });
-            });
-        });
+                    const domain = getDomein(url);
+                    // URLパースに失敗した場合、""を返す
+                    // そのままだと、https://www.google.com/s2/faviconsが400になるので、空文字を渡す
+                    const encode_domain = (domain === "") ? encodeURI(" ") : encodeURI(domain);
+                    const encode_url = escape_html(url);
+                    const encode_title = escape_html(tab.title);
+                    return `
+                        <li>
+                            <img src="https://www.google.com/s2/favicons?domain=${encode_domain}" alt="${encode_title}"/>
+                            <a href="${encode_url}" class="tab_link" data-url="${encode_url}" data-title="${encode_title}">${encode_title}</a>
+                        </li>`;
+                }).join("\n");
 
-        console.log(results);
+                const insertHtml = `
+                    <div class="session uk-card-default">
+                        <div class="uk-card-header">
+                            <h4 class="uk-card-title uk-margin-remove-bottom">セッション${i + 1}</h4>
+                            <p class="uk-text-meta uk-margin-remove-top uk-margin-remove-bottom">${tabs.length}個のタブ</p>
+                            <p class="uk-text-meta uk-margin-remove-top">セッションID: ${session.window.sessionId}</p>
+                        </div>
+                        <div class="uk-card-body">
+                            <ul>${tabs_html}</ul>
+                        </div>
+                    </div>`;
+                main.insertAdjacentHTML('beforeend', insertHtml);
+            }
+        });
     });
-};
+    console.log(results);
+}
