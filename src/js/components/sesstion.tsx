7f75e4225c5610aa6e1893f3c308e51d2ec81744
ik-fib <ik11235@gmail.com>
2021-08-25 22:45:29 +0900

    セッション内部すべてのリンクを開くメソッド追加

--- src/js/components/sesstion.tsx
+++ src/js/components/sesstion.tsx
@@ -7,10 +7,31 @@ interface SessionProps {
 }
 
 const Session: React.FC<SessionProps> = (props) => {
-
     const window = props.Session.window!;
     const tabs = window.tabs!;
     const sessionId = window.sessionId!;
+
+    const openAllTab = () => {
+        chrome.windows.create((window) => {
+            for (let i = 0; i < tabs.length; i++) {
+                const tab = tabs[i]!;
+                if (i == 0) {
+                    const nowTabs = window!.tabs!
+                    console.log(nowTabs);
+                    console.log("now");
+                    if (nowTabs.length > 0) {
+                        // @ts-ignore
+                        const tabId = nowTabs[0].id!;
+                        const properties = {url: tab.url, active: false}
+                        chrome.tabs.update(tabId, properties);
+                    }
+                } else {
+                    const properties = {windowId: window!.id, url: tab.url, active: false}
+                    chrome.tabs.create(properties);
+                }
+            }
+        });
+    };
 
     return (
         <div className="session uk-card-default">
@@ -20,7 +41,8 @@ const Session: React.FC<SessionProps> = (props) => {
                 <p className="uk-text-meta uk-margin-remove-top">セッションID: {sessionId}</p>
                 <div className="uk-grid">
                     <div className="uk-width-auto"><span
-                        className="session_tab_link_open uk-link">このセッションのすべてのリンクを開く</span>
+                        className="session_tab_link_open uk-link"
+                        onClick={openAllTab}>このセッションのすべてのリンクを開く</span>
                     </div>
                     <div className="uk-width-expand"/>
                 </div>
